2a53e6ee8705478b6a106552506f667d56d5cc21
Erica L. McCormick <ericamcc@stanford.edu>
2023-03-02 16:14:17 -0800

    initial add of pset4

---
+++ pset4/pset4.py
@@ -0,0 +1,29 @@
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+
+
+
+
+
+
+
+
+def main():
+    pass
+
+
+
+# goal: x: internal leaf CO2 concentration
+# y: photosynthesis rates (Aj, Ac, An)
+# Ac: rubisco liited (rate of assimilation)
+# Aj: light limited
+# An: net as
+
+
+
+if __name__ == '__main__':
+    main()
